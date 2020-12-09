76e846059e1b2f1a38d69e929c073b225cc727da
Ryan Skraba <rskraba@talend.com>
2020-12-09 14:38:33 +0100

    fix: Delete duplicated component code

--- processing-python3/src/main/java/org/talend/components/processing/python3/package-info.java
+++
@@ -1,6 +0,0 @@
-@Icon(value = Icon.IconType.CUSTOM, custom = "python")
-@Components(family = "Processing")
-package org.talend.components.processing.python3;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
